bc9dee0f3594fc4c51d04a40500e3bfe673d2801
Fuart Madnurak <85119856+ablazeArt@users.noreply.github.com>
2022-07-01 02:32:59 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 libary to use SQLite database
-db = os.environ.get('DATABASE_URL')
+db = SQL("sqlite:///todo.db")
 
 @app.after_request
 def after_request(response):
