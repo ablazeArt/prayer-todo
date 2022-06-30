4cc4c358afea92dc592658f619325f2c0b82a16a
Fuart Madnurak <85119856+ablazeArt@users.noreply.github.com>
2022-07-01 00:59:27 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,159 @@
+import os
+import json
+import requests
+
+from cs50 import SQL
+from tempfile import mkdtemp
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templaets are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 libary to use SQLite database
+db = SQL("sqlite:///todo.db")
+
+def getApp():
+    return app
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def index():
+    url = "https://dailyprayer.abdulrcs.repl.co/api/bangkok"
+    response = requests.get(url)
+    DATA = response.json()
+    #Send TODO data that user type in to index.html
+    return render_template("prayer.html", data = DATA)
+
+
+@app.route ("/todo", methods=["GET", "POST"])
+@login_required
+def todo():
+    if request.method == "POST":
+
+        # Add the user's entry into the database
+        session_user_id = session["user_id"]
+        task = request.form.get("task")
+        #Check if user provided input
+        if not task:
+            return apology("must provide task!", 400)
+
+        #Remember birthdays
+        db.execute("INSERT INTO todo (person_id, task) VALUES(?, ?)", session_user_id, task)
+        return redirect("/todo")
+
+    else:
+        # Display the entries in the database on index.html
+        session_user_id = session["user_id"]
+        TODO = db.execute("SELECT * FROM todo WHERE person_id = ?", session_user_id)
+        #Send TODO data that user type in to index.html
+        return render_template("index.html", todo = TODO)
+
+
+
+
+@app.route ("/login", methods=["GET", "POST"])
+def login():
+
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST (Login form))
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username or not password:
+            return apology("must provide username and password", 400)
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/todo")
+    
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """"Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/todo")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        #Ensure username was submitted
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username or not password or not confirmation:
+            return apology("must provide information", 400)
+
+        #define usernamedatabase to check if username already exist or not
+        database = db.execute("SELECT username FROM users WHERE username = ?", username)
+        username_database = "a"
+        for i in range(len(database)):
+            username_database = database[i]["username"]
+        if username == username_database:
+            return apology("username already exist")
+
+        #Ensure password and confirmation are correct
+        elif password != confirmation:
+            return apology("password and confirmation aren't match")
+
+        #get information from user to database
+
+        #use hash function to generate password to hash before insert into database
+        hash = generate_password_hash(password)
+        registrants = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+        return render_template("login.html")
+
+    else:
+        return render_template("register.html")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    #Delete birthdays
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM todo WHERE id = ?", id)
+    return redirect("/todo")
---
+++ static/main.js
@@ -0,0 +1,113 @@
+function display() {
+    var refresh = 1000;
+    myTime = setTimeout('display_time()',refresh);
+}
+       
+function display_time() {
+    const d = new Date();
+    let text = d.toLocaleTimeString('en-US', {hour12: false});
+    document.getElementById("time").innerHTML = text;
+    display();
+}
+
+
+var start = document.getElementById('control');
+var reset = document.getElementById('reset');
+
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
+
+var h_b = document.getElementById("hour_b");
+var m_b = document.getElementById("minute_b");
+var s_b = document.getElementById("sec_b");
+
+//store a reference to the startTimer variable
+var startTimer = null;
+var statusTimer = false;
+start.addEventListener('click', function(){
+    //initialize the variable
+    if (statusTimer === false) {
+        startTime();
+    } else {
+        stopTime();
+    }
+})
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+
+    h_b.value = 0;
+    m_b.value = 0;
+    s_b.value = 0;
+    //stop the timer after pressing "reset"
+    stopTime()
+})
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(h.value == 0 && m.value == 0 && s.value == 1){
+        s.value--;
+        play();
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    } 
+
+     //Break Timer Countdown
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        if(h_b.value == 0 && m_b.value == 0 && s_b.value == 0){
+            h_b.value = 0;
+            m_b.value = 0;
+            s_b.value = 0;
+        } else if(h_b.value == 0 && m_b.value == 0 && s_b.value == 1){
+            s_b.value--;
+            play();
+            stopTime();
+        } else if(s_b.value != 0){
+            s_b.value--;
+        } else if(m_b.value != 0 && s_b.value == 0){
+            s_b.value = 59;
+            m_b.value--;
+        } else if(h_b.value != 0 && m_b.value == 0){
+            m_b.value = 60;
+            h_b.value--;
+        }
+    } 
+}
+
+
+function startTime(){
+    control.innerHTML = '<span>Pause</span>'
+    control.classList.add("timer__btn--stop")
+    control.classList.remove("timer__btn--start")
+    statusTimer = true 
+    startTimer = setInterval(function() {
+        timer();
+    }, 1000);
+}
+
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopTime() {
+    control.innerHTML = '<span>Start</span>'
+    control.classList.add("timer__btn--start")
+    control.classList.remove("timer__btn--stop")
+    statusTimer = false
+    clearInterval(startTimer);
+}
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
