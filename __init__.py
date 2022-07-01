9145308182a2f30bc8eb4f974fc03339aa917655
Fuart Madnurak <85119856+ablazeArt@users.noreply.github.com>
2022-07-02 02:17:28 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,10 +27,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 libary to use SQLite database
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-db = uri
+db = os.environ.get('DATEBASE_URL')
 
 # Set time
 time_today = dt.datetime.now()
