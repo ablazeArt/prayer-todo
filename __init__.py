38d373e370593a52cecb555e3a2fa20b3e2e2b78
Fuart Madnurak <85119856+ablazeArt@users.noreply.github.com>
2022-07-02 02:35:10 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 libary to use SQLite database
-db = os.environ.get('DATEBASE_URL')
+db = SQL("sqlite:///todo.db")
 
 # Set time
 time_today = dt.datetime.now()
